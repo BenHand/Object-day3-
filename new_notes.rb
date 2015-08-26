61ffcb79d25777fab0e128f9536403c65cb91db0
Chris McCammon <cmccammon@gmail.com>
2015-08-26 13:37:05 -0500

    finished fizzbuzz

--- new_notes.rb
+++ new_notes.rb
@@ -1,16 +1,15 @@
 
 
 def fizzbuzz(n)
-  return 'fizzbuzz' if (n % 15).zero?  # => false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, fa...
-  return 'buzz' if (n % 5).zero?       # => false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, true, f...
-  return 'fizz' if (n % 3).zero?       # => false, false, true, false, true, false, false, true, false, true, false, false, false, false, true, false, true, false, false, true, false, true, false, false, false, false, true, false, true, false, false, true, false, true, false, false, false, false, true, false, true, false, false, true, false, true, false, false, false, false, true, false, true, false, false, true, false, true, false, false, false, false, true, false, true, false, false, true, ...
-  n                                    # => 1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19, 22, 23, 26, 28, 29, 31, 32, 34, 37, 38, 41, 43, 44, 46, 47, 49, 52, 53, 56, 58, 59, 61, 62, 64, 67, 68, 71, 73, 74, 76, 77, 79, 82, 83, 86, 88, 89, 91, 92, 94, 97, 98
-
+  return 'fizzbuzz' if (n % 15).zero?
+  return 'buzz' if (n % 5).zero?
+  return 'fizz' if (n % 3).zero?
+  n
 end
 
-1.upto(100) do |n|   # => 1
-  puts fizzbuzz (n)  # => nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, n...
-end                  # => 1
+1.upto(100) do |n|
+  puts fizzbuzz (n)
+end
 
 
 # >> 1
--- object-fun-2.rb
+++ object-fun-2.rb
@@ -1,44 +1,75 @@
-class Person
-  def initialize(name)
-    @name = name        # => "bob", "jil", "tom", "tim", "Sam", "eric"
+# class Person
+#   def initialize(name)
+#     @name = name        # => "bob", "jil", "tom", "tim", "Sam", "eric"
+#   end
+
+#   def get_name
+#     @name.capitalize  # => "Bob", "Jil", "Tom", "Tim", "Sam", "Eric"
+#   end
+
+#   def set_name(new_name)
+#     @name = new_name      # => "Jan"
+#   end
+
+# end
+
+
+# # Claasses are FActories (nouns)
+# # Their methods are Actions (Verbs)
+# # Thier instance variable are properties (data or State)
+
+
+# person1 = Person.new("bob")  # => #<Person:0x007fc521191840 @name="bob">
+# person2 = Person.new("jil")  # => #<Person:0x007fc521191548 @name="jil">
+
+# person1.get_name  # => "Bob"
+# person2.get_name  # => "Jil"
+
+# person1.set_name("Jan")  # => "Jan"
+
+# student_records = Array.new    # => []
+
+# student_records.push(Person.new("tom"))   # => [#<Person:0x007fc521190c10 @name="tom">]
+# student_records.push(Person.new("tim"))   # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">]
+# student_records.push(Person.new("Sam"))   # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">]
+# student_records.push(Person.new("eric"))  # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">, #<Person:0x007fc5211903c8 @name="eric">]
+
+# student_records.each do |student|  # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">, #<Person:0x007fc5211903c8 @name="eric">]
+#   puts student.get_name            # => nil, nil, nil, nil
+# end                                # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">, #<Person:0x007fc5211903c8 @name="eric">]
+
+# # >> Tom
+# # >> Tim
+# # >> Samr
+# # >> Eric
+
+class Animal
+
+  def breathe
+    "inhales"  # => "inhales"
   end
 
-  def get_name
-    @name.capitalize  # => "Bob", "Jil", "Tom", "Tim", "Sam", "Eric"
+   def speak
+    "grrrrrhsssg"  # => "grrrrrhsssg"
   end
-
-  def set_name(new_name)
-    @name = new_name      # => "Jan"
-  end
-
 end
 
+class Cat < Animal
+  # def speak
+  #   "meow"          # => "meow"
+  # end
+end
 
-# Claasses are FActories (nouns)
-# Their methods are Actions (Verbs)
-# Thier instance variable are properties (data or State)
+cat = Cat.new  # => #<Cat:0x007f97b1056b08>
+cat.speak      # => "grrrrrhsssg"
+cat.breathe    # => "inhales"
 
 
-person1 = Person.new("bob")  # => #<Person:0x007fc521191840 @name="bob">
-person2 = Person.new("jil")  # => #<Person:0x007fc521191548 @name="jil">
 
-person1.get_name  # => "Bob"
-person2.get_name  # => "Jil"
 
-person1.set_name("Jan")  # => "Jan"
 
-student_records = Array.new    # => []
 
-student_records.push(Person.new("tom"))   # => [#<Person:0x007fc521190c10 @name="tom">]
-student_records.push(Person.new("tim"))   # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">]
-student_records.push(Person.new("Sam"))   # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">]
-student_records.push(Person.new("eric"))  # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">, #<Person:0x007fc5211903c8 @name="eric">]
 
-student_records.each do |student|  # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">, #<Person:0x007fc5211903c8 @name="eric">]
-  puts student.get_name            # => nil, nil, nil, nil
-end                                # => [#<Person:0x007fc521190c10 @name="tom">, #<Person:0x007fc521190a08 @name="tim">, #<Person:0x007fc521190710 @name="Sam">, #<Person:0x007fc5211903c8 @name="eric">]
 
-# >> Tom
-# >> Tim
-# >> Samr
-# >> Eric
+
+
