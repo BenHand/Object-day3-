724c7bf3892b4161c610f2744f82d23000c42ac8
Chris McCammon <cmccammon@gmail.com>
2015-08-26 12:02:06 -0500

    todays lectures

---
+++ class_fun.rb
@@ -0,0 +1,32 @@
+class  Student
+
+  def initialize
+    @bed_time = "didn't sleep yet"  # => "didn't sleep yet", "didn't sleep yet"
+    @awakw_time = "not yet"         # => "not yet", "not yet"
+  end
+
+  def wakes_up
+    @awake_time = Time.now                                       # => 2015-08-26 11:15:20 -0500
+    "Went to bed at #{@bed_time} and woke up at #{@awake_time}"  # => "Went to bed at didn't sleep yet and woke up at 2015-08-26 11:15:20 -0500"
+  end
+
+  def sleep
+    @bed_time = Time.now            # => 2015-08-26 11:15:20 -0500
+     "Goes to bed at #{@bed_time}"  # => "Goes to bed at 2015-08-26 11:15:20 -0500"
+  end
+
+  def study
+      "Reads lots and lots of books"  # => "Reads lots and lots of books"
+  end
+end
+
+jacob = Student.new  # => #<Student:0x007f88b99919b8 @bed_time="didn't sleep yet", @awakw_time="not yet">
+sarah = Student.new  # => #<Student:0x007f88b99915f8 @bed_time="didn't sleep yet", @awakw_time="not yet">
+
+sarah.wakes_up  # => "Went to bed at didn't sleep yet and woke up at 2015-08-26 11:15:20 -0500"
+sarah.study     # => "Reads lots and lots of books"
+jacob.sleep     # => "Goes to bed at 2015-08-26 11:15:20 -0500"
+
+
+
+
